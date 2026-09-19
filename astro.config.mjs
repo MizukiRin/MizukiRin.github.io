f610bc63d2bae0c70e9b1928c95adb58a49fc45c
MizukiRin <rinmizuki123@Gmail.com>
2026-09-19 23:46:39 +0800

    优化md格式渲染

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://MizukiRin.github.io', // 您的站点域名
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
